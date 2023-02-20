9e33f797cef693854dc8cc91d2bffafc6adbae84
Milagro Scardigno <mdscardigno@gmail.com>
2023-02-20 15:30:22 -0500

    getting the list of all breeds

--- Program.cs
+++ Program.cs
@@ -1,19 +1,46 @@
 ﻿using System;
 using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace APIClient
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
+
+            //Create a menu that will retrieve data from the Dog API
+            //https://dog.ceo/dog-api/
+
+            //list all dogs breeds
+            //https://dog.ceo/api/breeds/list/all
+
+            //list all sub-breeds for a breed
+            //https://dog.ceo/api/breed/hound/list
+
+            //list random image for random breed
+            //https://dog.ceo/api/breeds/image/random
+
+            //list random image for a breed
+            //https://dog.ceo/api/breed/hound/images/random
+
+            //list image for a sub-breed
+            //https://dog.ceo/api/breed/hound/images
             Console.WriteLine("Dogs API Client");
+
+
+            // var keepGoing = true;
+            // while (keepGoing)
+            // {
+
+
+            // }
 
             var client = new HttpClient();
 
-            var responseAsString = client.GetStringAsync("https://lyricsovh.docs.apiary.io/#reference/0/lyrics-of-a-song/search");
+            var responseAsString = await client.GetStringAsync("https://dog.ceo/api/breeds/list/all");
 
-            Console.WriteLine(responseAsString.Result);
+            Console.WriteLine(responseAsString);
         }
     }
 }
