21c17c782a3772cba0e2d547e5557c2a0fecce30
Milagro Scardigno <mdscardigno@gmail.com>
2023-02-20 19:40:26 -0500

    changed server name to get results

--- Lyrics.cs
+++ Lyrics.cs
@@ -2,11 +2,18 @@ using System;
 using System.Net.Http;
 using System.Drawing;
 using System.Text.Json.Serialization;
+using System.Collections.Generic;
+
 namespace APIClient
 {
     public class Lyrics
     {
         [JsonPropertyName("lyrics")]
         public string SongLyrics { get; set; }
+
+        public static implicit operator Lyrics(List<Lyrics> v)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
